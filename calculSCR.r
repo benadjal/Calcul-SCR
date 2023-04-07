83b98cb8336ea2eed7aad74ab849e4a54ea0e75d
Adame Benadjal <adame.benadjal@gmail.com>
2023-04-07 16:08:49 +0200

    Calcul de la VM de choque

--- calculSCR.r
+++ calculSCR.r
@@ -7,10 +7,11 @@ Base_Actif <- read.csv2(file = "./baseActifs")
 
 #Choc sur les actifs
 choc_immo <- 0.3
+choc_action <- 0.39
 
 #Calcul de la VM de choquee
 Base_Actif %>% 
-  mutate(VM_choc = VM * (1- choc_immo))
+  mutate(VM_choc = ifelse(Classe_actif == "Immobilier", VM * (1- choc_immo), ifelse(Classe_actif == "Action", VM * (1- choc_action), VM)))
 
 #Calcul du SCR brut
 SCR_brut <- sum(Base_Actif$VM)-sum(Base_Actif$VM_choc)
